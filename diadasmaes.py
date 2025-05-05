7539888fd4d3d4f8c5bb2c25bd897e48a743deb9
Luiz Henrique <78453305+lhnl007hm@users.noreply.github.com>
2025-05-05 13:07:57 -0300

    Primeiro Commit

---
+++ diadasmaes.py
@@ -0,0 +1,30 @@
+import time
+
+mensagem = [
+    "Feliz Dia das Mães!",
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+    "Mãe, obrigado por tudo que você faz por mim.",
+    "Você é especial, forte e cheia de amor!",
+    "Te amo hoje e sempre! 💐",
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+    "Mamãe vocé é amor",
+    "É meu exemplo da vida",
+    "Eu te ofereço um Dolly",
+    "Com toda emoção",
+    "Feliz dia dos pais"
+]
+
+for linha in mensagem:
+    print(linha)
+    time.sleep(1.5)  # espera 1.5 segundos antes de mostrar a próxima linha
+
+# Criando um pequeno coração com caracteres
+print()
+print("    **     **")
+print("  ****** ******")
+print(" **************")
+print("  ************")
+print("   **********")
+print("     ******")
+print("      ****")
+print("       **")
